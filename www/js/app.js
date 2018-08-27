afbe32d85202c868a08d2264e7225ddebb9d4186
Sathwik <sathwikdec@gmail.com>
2018-08-27 13:25:19 -0400

    inital commit

--- www/js/app.js
+++ www/js/app.js
@@ -24,3 +24,75 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('splash', {
+      url: '/splash',
+      templateUrl: '../template/splash.html',
+      controller: 'splashCtrl'
+    })
+    .state('main', {
+      url: '/main',
+      templateUrl: '../template/main.html',
+      controller: 'mainCtrl'
+    })
+    .state('info', {
+      url: '/info',
+      templateUrl: '../template/info.html',
+      controller: 'infoCtrl'
+    });
+  $urlRouterProvider.otherwise('/splash');
+}])
+
+.controller('mainCtrl',['$scope', '$http', '$state', function($scope, $http, $state){
+  $scope.get = function () {
+    city = $scope.cityName;
+    $http.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=279b9caa6884de71bd56f55d055cc2b4')
+      .then(function (res) {
+        $scope.datas = res.data;
+        temp = $scope.datas.main.temp;
+        $scope.temperature = temp - 273.15;
+        console.log("Data reveived in 'res'");
+        window.localStorage.setItem('res', JSON.stringify(res));
+      },
+        function (err) {
+          console.log("ERROR: ", err);
+        });
+  }
+
+  // Toggle between Celsius and fahrenheit 
+  
+  $scope.changeTemp = function(){
+    console.log($scope.cToF)
+    if($scope.cToF === true){
+      $scope.temperature = temp - 273.15;
+    } else{
+      $scope.temperature = (temp * 1.8) - 459.67;
+    }
+    $scope.cToF = !$scope.cToF;
+  }
+  // Change screens when swipe up
+  $scope.changeInfo = function(){
+    console.log('main page --> info page');
+    $state.go('info');
+  }
+  // Change background based on the temperature
+  $scope.colorValue = 'red';
+}])
+
+.controller('splashCtrl', ['$state', '$timeout', function($state, $timeout){
+  console.log('Changing in 5 secs...');
+  $timeout(function(){
+    $state.go('main');
+  },5000);
+}])
+
+.controller('infoCtrl', ['$scope', '$state', function($scope, $state){
+  $scope.changeMain = function(){
+    console.log('info page --> main page');
+    $state.go('main');
+  }
+  $scope.datas = JSON.parse(window.localStorage.getItem('res')).data;
+  console.log($scope.datas);
+}])
