1caf9bffa33c7f3f76b352f49c9034dbfe3f91f1
Sathwik <sathwikdec@gmail.com>
2018-08-30 23:47:44 -0400

    additional css changes anf few tweaks

--- www/js/app.js
+++ www/js/app.js
@@ -42,50 +42,97 @@ angular.module('starter', ['ionic'])
       templateUrl: '../template/info.html',
       controller: 'infoCtrl'
     });
-  $urlRouterProvider.otherwise('/splash');
+  $urlRouterProvider.otherwise('/main');
 }])
 
-.controller('mainCtrl',['$scope', '$http', '$state', function($scope, $http, $state){
+.controller('mainCtrl',['$scope', '$http', '$state', '$ionicPopup', function($scope, $http, $state, $ionicPopup){
+  $scope.content = false;
   $scope.get = function () {
     city = $scope.cityName;
     $http.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=279b9caa6884de71bd56f55d055cc2b4')
       .then(function (res) {
         $scope.datas = res.data;
         temp = $scope.datas.main.temp;
+        temp_min = $scope.datas.main.temp_min;
+        temp_max = $scope.datas.main.temp_max;
+        weather = $scope.datas.weather[0].main;
+        changeImage(weather);
         $scope.temperature = temp - 273.15;
+        $scope.temperatureMin = temp_min - 273.15;
+        $scope.temperatureMax = temp_max - 273.15;
+        sunriseUnix = $scope.datas.sys.sunrise;
+        sunsetUnix = $scope.datas.sys.sunset;
+        convertTime(sunriseUnix,sunsetUnix);
         console.log("Data reveived in 'res'");
+        $scope.content = true;
         window.localStorage.setItem('res', JSON.stringify(res));
       },
         function (err) {
           console.log("ERROR: ", err);
+          $ionicPopup.alert({
+            cssClass: "alert",
+            templateUrl: '../template/alert.html',
+            okType: 'button-assertive'
+          });
+          $scope.content = false;
         });
   }
-
-  // Toggle between Celsius and fahrenheit 
   
+  // Display Weather Icon according to climate
+  var changeImage = function (weather){
+    console.log(weather);
+    $scope.imgUrl = '../img/clear.png';
+    if (weather === "Clouds") {
+      $scope.imgUrl = '../img/cloudy.png';
+    } else if (weather === "Clear") {
+      $scope.imgUrl = '../img/clear.png';
+    } else if (weather === "Rain") {
+      $scope.imgUrl = '../img/chance of rain.png';
+    }
+  }
+  
+
+  // convert UNIX time to UTC time
+  function convertTime(sunriseUnix,sunsetUnix){
+    sunriseTime = new Date(sunriseUnix * 1000);
+    sunsetTime = new Date(sunsetUnix * 1000);
+    $scope.sunrise = sunriseTime.toLocaleTimeString();
+    $scope.sunset = sunsetTime.toLocaleTimeString();
+  }
+  $scope.showInfo = function(){
+    $scope.swipe = true;
+  }
+
+  $scope.showOther = function(){
+    $scope.swipe = false;
+  }
+  // Toggle between Celsius and fahrenheit 
+  $scope.cToF = false;
   $scope.changeTemp = function(){
     console.log($scope.cToF)
     if($scope.cToF === true){
       $scope.temperature = temp - 273.15;
+      $scope.temperatureMin = temp_min - 273.15;
+      $scope.temperatureMax = temp_max - 273.15;
     } else{
       $scope.temperature = (temp * 1.8) - 459.67;
+      $scope.temperatureMin = (temp_min * 1.8) - 459.67;
+      $scope.temperatureMax = (temp_max * 1.8) - 459.67;
     }
     $scope.cToF = !$scope.cToF;
   }
   // Change screens when swipe up
-  $scope.changeInfo = function(){
-    console.log('main page --> info page');
-    $state.go('info');
-  }
-  // Change background based on the temperature
-  $scope.colorValue = 'red';
+  // $scope.changeInfo = function(){
+  //   console.log('main page --> info page');
+  //   $state.go('info');
+  // }
 }])
 
 .controller('splashCtrl', ['$state', '$timeout', function($state, $timeout){
   console.log('Changing in 5 secs...');
-  $timeout(function(){
-    $state.go('main');
-  },5000);
+  // $timeout(function(){
+  //   $state.go('main');
+  // },5000);
 }])
 
 .controller('infoCtrl', ['$scope', '$state', function($scope, $state){
